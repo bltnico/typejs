9c00ebfcee3c673f54ba594717363b872e6dffc5
bltnico <bellot.n@gmail.com>
2018-02-22 16:49:11 +0100

    [Build]

--- dist/type.js
+++ dist/type.js
@@ -13,10 +13,17 @@ exports.typeVar = typeVar;
 
 var _typeCheck = require('type-check');
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var TYPE_NAME_KEY = '__typeName';
+var TYPE_SCHEMA_PREFIX = '__type/';
 var DEFAULT_OPTIONS = {
   strict: true,
   fatal: false
+};
+var SPECIAL_TYPE = {
+  EQUAL: 'equal',
+  PROMISE: 'promise'
 };
 
 function getTypeName(schemaType) {
@@ -25,6 +32,44 @@ function getTypeName(schemaType) {
 
 function getOptions(options) {
   return _extends({}, DEFAULT_OPTIONS, options);
+}
+
+function getSpecialTypeSchemaKey(type) {
+  return '' + TYPE_SCHEMA_PREFIX + type;
+}
+
+function isSpecialTypeSchemaKey(object) {
+  if (!(0, _typeCheck.typeCheck)('Object', object)) {
+    return false;
+  }
+
+  return Object.keys(object).some(function (key) {
+    return !!key.startsWith(TYPE_SCHEMA_PREFIX);
+  });
+}
+
+function getSpecialTypeSchemaKeyName(object) {
+  var keys = Object.keys(object)[0];
+  return keys ? keys.split('/')[1] : null;
+}
+
+function checkSpecialTypeSchema(schemaType, element, options, key) {
+  switch (getSpecialTypeSchemaKeyName(schemaType[key])) {
+    case SPECIAL_TYPE.EQUAL:
+      var list = schemaType[key][getSpecialTypeSchemaKey(SPECIAL_TYPE.EQUAL)];
+      if (!list.includes(element[key])) {
+        return typeError(schemaType, element, options.fatal, key, 'Type/Equal', element[key].toString());
+      }
+      break;
+    case SPECIAL_TYPE.PROMISE:
+      var isPromise = Promise.resolve(element[key]) === element[key];
+      if (!isPromise) {
+        return typeError(schemaType, element, options.fatal, key, 'Type/Promise', '');
+      }
+      break;
+    default:
+      return true;
+  }
 }
 
 function error(message, fatalMode) {
@@ -68,7 +113,7 @@ function unknowKeyerror(schemaType, elementKey, fatalMode) {
 
 var defineType = exports.defineType = function defineType(name) {
   var schema = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-  return Object.freeze(_extends({ __typeName: name }, schema));
+  return Object.freeze(_extends(_defineProperty({}, TYPE_NAME_KEY, name), schema));
 };
 
 function type(schemaType) {
@@ -96,7 +141,9 @@ function type(schemaType) {
 
     elementKeys.forEach(function (key) {
       if (typeKeys.includes(key)) {
-        if ((0, _typeCheck.typeCheck)('Object', schemaType[key])) {
+        if (isSpecialTypeSchemaKey(schemaType[key])) {
+          checkSpecialTypeSchema(schemaType, element, options, key);
+        } else if ((0, _typeCheck.typeCheck)('Object', schemaType[key])) {
           type(schemaType[key])(element[key], options);
         } else {
           var isValid = (0, _typeCheck.typeCheck)(schemaType[key], element[key]);
@@ -145,13 +192,25 @@ function typeVar(schemaString) {
 var any = '*';
 var number = 'Number';
 var string = 'String';
-var boolean = 'Boolean';
+var date = 'Date';
+var bool = 'Boolean';
 var array = 'Array';
 var func = 'Function';
 var object = 'Object';
+var promise = _defineProperty({}, getSpecialTypeSchemaKey(SPECIAL_TYPE.PROMISE), true);
+var maybe = function maybe(type) {
+  return 'Maybe ' + type;
+};
+var equal = function equal() {
+  for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+    args[_key] = arguments[_key];
+  }
+
+  return _defineProperty({}, getSpecialTypeSchemaKey(SPECIAL_TYPE.EQUAL), args);
+};
 var oneOf = function oneOf() {
-  for (var _len = arguments.length, types = Array(_len), _key = 0; _key < _len; _key++) {
-    types[_key] = arguments[_key];
+  for (var _len2 = arguments.length, types = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
+    types[_key2] = arguments[_key2];
   }
 
   return types.join(' | ');
@@ -159,19 +218,24 @@ var oneOf = function oneOf() {
 var arrayOf = function arrayOf() {
   return '[' + oneOf.apply(undefined, arguments) + ']';
 };
-var maybe = function maybe(type) {
-  return 'Maybe ' + type;
+var objectStruct = function objectStruct() {
+  var childObject = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+  return _extends(_defineProperty({}, TYPE_NAME_KEY, childObject[TYPE_NAME_KEY] || 'Type/objectStruct'), childObject);
 };
 
 var t = exports.t = {
   any: any,
   number: number,
   string: string,
-  boolean: boolean,
+  date: date,
+  bool: bool,
   array: array,
   func: func,
   object: object,
+  promise: promise,
+  maybe: maybe,
+  equal: equal,
   arrayOf: arrayOf,
   oneOf: oneOf,
-  maybe: maybe
+  objectStruct: objectStruct
 };
